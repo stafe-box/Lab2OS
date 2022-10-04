e89e56015fed4d74606c71f148a2e081d3a32c7d
stafe <stafe@CHROMATIC>
2022-10-04 20:06:59 +0500

    Добавьте файлы проекта.

---
+++ Lab2OS/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Lab2OS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GenerateButton.Location = new System.Drawing.Point(32, 25);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(256, 71);
+            this.GenerateButton.TabIndex = 0;
+            this.GenerateButton.Text = "Generate";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 123);
+            this.Controls.Add(this.GenerateButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(339, 162);
+            this.MinimumSize = new System.Drawing.Size(339, 162);
+            this.Name = "Form1";
+            this.Text = "Лбараторная №3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenerateButton;
+    }
+}
+
---
+++ Lab2OS/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2OS
+{
+    public partial class Form1 : Form
+    {
+        protected string _way = "Buffer.txt";
+        private Thread Thread1, Thread2;
+        private Mutex Mutex;
+        public Form1()
+        {
+            InitializeComponent();
+            Mutex = new Mutex();
+        }
+
+        private void GenerateNewLine()
+        {
+            if (Mutex.WaitOne())
+            {
+                int num_letters = 10;
+                char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890+".ToCharArray();
+
+                // Создаем генератор случайных чисел.
+                Random rand = new Random();
+
+                // Делаем слова.
+                string word = "";
+                for (int j = 1; j <= num_letters; j++)
+                {
+                    // Выбор случайного числа от 0 до количества символов
+                    // для выбора буквы из массива букв.
+                    int letter_num = rand.Next(0, letters.Length - 1);
+                    // Добавить слово.
+                    word += letters[letter_num];
+                }
+                //MessageBox.Show(word);
+                StreamWriter sw = File.CreateText(_way);
+                sw.WriteLine(word);
+                sw.Close();
+                sw.Dispose();
+                //Mutex.Dispose();
+                Mutex.ReleaseMutex();
+
+            }
+        }
+
+        private void ReadAndShow()
+        {
+            if (Mutex.WaitOne())
+            {
+                StreamReader sr = File.OpenText(_way);
+                string line = sr.ReadToEnd();
+
+                MessageBox.Show($"Сгенерированна строка: {line}");
+                sr.Close();
+                sr.Dispose();
+                //Mutex.Dispose();
+                Mutex.ReleaseMutex();
+            }
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            Thread1 = new Thread(GenerateNewLine);
+            Thread2 = new Thread(ReadAndShow);
+
+            Thread1.Start();
+            Thread2.Start();
+        }
+    }
+}
